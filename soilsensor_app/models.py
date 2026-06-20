dd7d5fb0d6471ebd40ce6f35b31cc241d573d542
remmy-web <137243748+remmy-web@users.noreply.github.com>
2026-06-20 11:31:01 +0300

    updated sensor model string representation

--- soilsensor_app/models.py
+++ soilsensor_app/models.py
@@ -5,6 +5,9 @@ class Sensor(models.Model):
     owner_name=models.CharField(max_length=20, null=True, blank=True)
     contact= models.CharField(max_length=10, null=True, blank=True)
 
+    def __str__(self):
+        return f"Sensor_ID:{self.id}"
+
 class Sample(models.Model):
     sensor= models.ForeignKey(Sensor,on_delete = models.SET_NULL, null=True,)
     nitrogen = models.FloatField(null=True, blank=True)
